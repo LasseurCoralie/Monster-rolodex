7fc66271b1dc82066c366ba2e69786c822fb8d3f
LasseurCoralie <coralielasseur@gmail.com>
2020-02-21 14:50:27 +0100

    creation card list et ajout css

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component.jsx';
+
+
 import './App.css';
+
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Franckenstein',
-          id: 1,
-        },
-        {
-          name: 'Dracula',
-          id: 2,
-        },
-        {
-          name: 'Zombie',
-          id: 3,
-        },
-      ]
+      monsters: []
     }
-  }  
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
 
   render() {
     return (
     <div className="App">
-      <header className="App-header">
-        {this.state.monsters.map(monster => <h1 key={ monster.id }>{ monster.name }</h1>)}
-        <button onClick={() => this.setState({ string: 'Coucou Machin' })}>boutton</button>
-      </header>
+    <CardList name="Coco">
+      {this.state.monsters.map(monster => <h1 key={ monster.id }>{ monster.name }</h1>)}
+    </CardList>
     </div>
     );
   }
