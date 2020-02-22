726a41cfc83a4c8b83275ececdcb9443e6bdcad6
LasseurCoralie <coralielasseur@gmail.com>
2020-02-22 15:42:29 +0100

    ajout css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component.jsx';
-
+import { SearchBox } from './components/search-box/search-box.component.jsx';
 
 import './App.css';
 
@@ -13,6 +13,7 @@ class App extends Component {
       monsters: [],
       searchField: '',
     }
+
   }
 
   componentDidMount() {
@@ -21,20 +22,24 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (event) => {
+    this.setState({ searchField: event.target.value })
+  }
+
   render() {
 
     const { monsters, searchField } = this.state; // destructuration et application des states dans var monsters et searchField
     const filteredMonsters = monsters.filter(monster => 
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-    )
+    );
     // fonction filteredMonster : on filtre les monstres en prenant comme argument un monstre, on passe son nom en lower case et on utilise la méthode includes qui permet de faire une lecture dans le name et de voir si la valeur du seach field est égale à une partie du nom des monstres. filteredMonsters permet d'afficher les monstres filtrés. 
 
     return (
     <div className="App">
-      <input 
-        type='search' 
-        placeholder='search monsters' 
-        onChange={event => this.setState({ searchField: event.target.value })}
+    <h1>Monsters Rolodex</h1>
+      <SearchBox 
+        placeholder="search monsters"
+        handleChange={this.handleChange}
       />
       <CardList monsters={filteredMonsters} />
     </div>
